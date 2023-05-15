f81b444e70fe4fbc1b655c8f5c87fcce59f59f9d
L-Dragon5 <lildragon555@gmail.com>
2023-05-15 12:37:26 -0400

    Add Instagram login provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import InstagramProvider from 'next-auth/providers/instagram';
 
 import prisma from '../../../lib/prisma';
 
@@ -7,7 +8,12 @@ const authHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options = {
-  providers: [],
+  providers: [
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    }),
+  ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
