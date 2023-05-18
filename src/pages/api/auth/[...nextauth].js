e47773e1d4bd9099fff1924c9f06aa2d37212a5f
L-Dragon5 <lildragon555@gmail.com>
2023-05-18 00:32:36 -0400

    Add Google login Provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
 
 import InstagramProvider from '@/lib/instagram-provider';
 
@@ -15,14 +16,24 @@ const options = {
     //   clientId: process.env.INSTAGRAM_CLIENT_ID,
     //   clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
     // }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    signIn: async ({ account, profile }) => {
+      if (account.provider === 'google') {
+        return profile.email_verified && profile.email.endsWith('@example.com');
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
     session: async ({ session, user }) => {
       return {
         ...session,
