fbe36da80bdb9c9f36a5ea437b48afbca003868f
L-Dragon5 <lildragon555@gmail.com>
2023-05-18 01:14:34 -0400

    Fix sign in callback preventing login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -30,7 +30,7 @@ const options = {
   callbacks: {
     signIn: async ({ account, profile }) => {
       if (account.provider === 'google') {
-        return profile.email_verified && profile.email.endsWith('@example.com');
+        return profile.email_verified;
       }
       return true; // Do different verification for other providers that don't have `email_verified`
     },
